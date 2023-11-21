c6e1b4eb776058ce224cd5663dd7b4bb678d9c47
OK_Kehan <tang.keh@northeastern.edu>
2023-11-20 20:39:50 -0800

    set up the JDA

---
+++ discord_bot/src/main/java/org/example/Bot.java
@@ -0,0 +1,10 @@
+package org.example;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+    public static void main( String[] args )
+    {
+        JDA jda = JDABuilder.createDefault("MTE3NjIzMzQyOTk5MDM5NTkzNg.G64Xyq.P8l0KlaUbLruQzUZp0ugtPwVZls7wI1MeuLmYw").build();
+    }
+}
